593d1edb9b7c28b8aa6a45ad221b09c36582606d
Johan Henriksson <johanhenriksson@live.com>
2019-05-06 20:59:31 +0200

    support scaled high-dpi displays

--- engine/window.go
+++ engine/window.go
@@ -1,130 +1,134 @@
 package engine
 
 import (
-    "log"
-    "time"
-    "runtime"
+	"log"
+	"runtime"
+	"time"
 
-    "github.com/go-gl/gl/v4.1-core/gl"
-    "github.com/go-gl/glfw/v3.1/glfw"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
 )
 
 func init() {
-    /* GLFW event handling must run on the main OS thread */
-    runtime.LockOSThread()
+	/* GLFW event handling must run on the main OS thread */
+	runtime.LockOSThread()
 }
 
 type UpdateCallback func(float32)
 type RenderCallback func(*Window, float32)
 
 type Window struct {
-    Wnd             *glfw.Window
-    Width           int
-    Height          int
-    updateCb        UpdateCallback
-    renderCb        RenderCallback
-    maxFrameTime    float64
-    lastFrameTime   float64
+	Wnd           *glfw.Window
+	Width         int
+	Height        int
+	updateCb      UpdateCallback
+	renderCb      RenderCallback
+	maxFrameTime  float64
+	lastFrameTime float64
 }
 
 func CreateWindow(title string, width int, height int) *Window {
-    if err := glfw.Init(); err != nil {
-        log.Fatalln("Failed to initialize glfw:", err)
-    }
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("Failed to initialize glfw:", err)
+	}
 
-    /* GLFW Window settings */
-    glfw.WindowHint(glfw.ContextVersionMajor, 4)
-    glfw.WindowHint(glfw.ContextVersionMinor, 1)
-    glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-    glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	/* GLFW Window settings */
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
-    window, err := glfw.CreateWindow(width, height, title, nil, nil)
-    if err != nil {
-        panic(err)
-    }
-    window.MakeContextCurrent()
-    glfw.SwapInterval(2);
+	window, err := glfw.CreateWindow(width, height, title, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+	glfw.SwapInterval(2)
 
-    /* Initialize OpenGL */
-    if err := gl.Init(); err != nil {
-        panic(err)
-    }
+	/* Initialize OpenGL */
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
 
-    window.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
-    window.SetKeyCallback(KeyCallback)
-    window.SetCursorPosCallback(MouseMoveCallback)
-    window.SetMouseButtonCallback(MouseButtonCallback)
+	window.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
+	window.SetKeyCallback(KeyCallback)
+	window.SetCursorPosCallback(MouseMoveCallback)
+	window.SetMouseButtonCallback(MouseButtonCallback)
 
-    w := &Window {
-        Width:          width,
-        Height:         height,
-        Wnd:            window,
-        maxFrameTime:   0.0,
-        lastFrameTime:  glfw.GetTime(),
-    }
-    w.SetMaxFps(60)
-    return w
+	w := &Window{
+		Width:         width,
+		Height:        height,
+		Wnd:           window,
+		maxFrameTime:  0.0,
+		lastFrameTime: glfw.GetTime(),
+	}
+	w.SetMaxFps(60)
+	return w
 }
 
 func (wnd *Window) Closed() bool {
-    return wnd.Wnd.ShouldClose()
+	return wnd.Wnd.ShouldClose()
 }
 
 func (wnd *Window) SetMaxFps(fps int) {
-    wnd.maxFrameTime = 1.0 / float64(fps)
+	wnd.maxFrameTime = 1.0 / float64(fps)
 }
 
 func (wnd *Window) LockCursor() {
-    wnd.Wnd.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
+	wnd.Wnd.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
 }
 
 func (wnd *Window) ReleaseCursor() {
-    wnd.Wnd.SetInputMode(glfw.CursorMode, glfw.CursorNormal)
+	wnd.Wnd.SetInputMode(glfw.CursorMode, glfw.CursorNormal)
 }
 
 func (wnd *Window) SetRenderCallback(cb RenderCallback) {
-    wnd.renderCb = cb
+	wnd.renderCb = cb
 }
 
 func (wnd *Window) SetUpdateCallback(cb UpdateCallback) {
-    wnd.updateCb = cb
+	wnd.updateCb = cb
 }
 
 func (wnd *Window) Loop() {
-    for !wnd.Closed() {
-        t  := glfw.GetTime()
-        dt := float32(t - wnd.lastFrameTime)
-        wnd.lastFrameTime = t
+	for !wnd.Closed() {
+		t := glfw.GetTime()
+		dt := float32(t - wnd.lastFrameTime)
+		wnd.lastFrameTime = t
 
-        UpdateMouse(dt)
-        if MouseDown(MouseButton1) {
-            wnd.LockCursor()
-        } else {
-            wnd.ReleaseCursor()
-        }
+		UpdateMouse(dt)
+		if MouseDown(MouseButton1) {
+			wnd.LockCursor()
+		} else {
+			wnd.ReleaseCursor()
+		}
 
-        if wnd.updateCb != nil {
-            wnd.updateCb(dt)
-        }
+		if wnd.updateCb != nil {
+			wnd.updateCb(dt)
+		}
 
-        if wnd.renderCb != nil {
-            wnd.renderCb(wnd, dt)
-        }
+		if wnd.renderCb != nil {
+			wnd.renderCb(wnd, dt)
+		}
 
-        wnd.EndFrame()
-        if wnd.maxFrameTime > 0 {
-            elapsed := glfw.GetTime() - t
-            dur := wnd.maxFrameTime - elapsed
-            time.Sleep(time.Duration(dur) * time.Second)
-        }
-    }
+		wnd.EndFrame()
+		if wnd.maxFrameTime > 0 {
+			elapsed := glfw.GetTime() - t
+			dur := wnd.maxFrameTime - elapsed
+			time.Sleep(time.Duration(dur) * time.Second)
+		}
+	}
 }
 
 func (wnd *Window) EndFrame() {
-    wnd.Wnd.SwapBuffers()
-    glfw.PollEvents()
+	wnd.Wnd.SwapBuffers()
+	glfw.PollEvents()
 }
 
 func (wnd *Window) Terminate() {
-    glfw.Terminate()
+	glfw.Terminate()
+}
+
+func (wnd *Window) GetBufferSize() (int, int) {
+	return wnd.Wnd.GetFramebufferSize()
 }
--- goworld.go
+++ goworld.go
@@ -18,124 +18,127 @@ package main
  */
 
 import (
-    "fmt"
-    "github.com/johanhenriksson/goworld/game"
-    "github.com/johanhenriksson/goworld/engine"
-    "github.com/johanhenriksson/goworld/render"
+	"fmt"
 
-    opensimplex "github.com/ojrac/opensimplex-go"
+	"github.com/johanhenriksson/goworld/engine"
+	"github.com/johanhenriksson/goworld/game"
+	"github.com/johanhenriksson/goworld/render"
+
+	opensimplex "github.com/ojrac/opensimplex-go"
 )
 
 const (
-    WIDTH  = 1600
-    HEIGHT = 1000
+	WIDTH  = 800
+	HEIGHT = 600
 )
 
 func main() {
-    app := engine.NewApplication("voxels", WIDTH, HEIGHT)
+	app := engine.NewApplication("voxels", WIDTH, HEIGHT)
 
-    /* grab a reference to the geometry render pass */
-    geom_pass := app.Render.Get("geometry").(*engine.GeometryPass)
-    light_pass := app.Render.Get("light").(*engine.LightPass)
+	/* grab a reference to the geometry render pass */
+	geom_pass := app.Render.Get("geometry").(*engine.GeometryPass)
+	light_pass := app.Render.Get("light").(*engine.LightPass)
 
-    /* create a camera */
-    app.Scene.Camera = engine.CreateCamera(-3,2,-3, WIDTH, HEIGHT, 65.0, 0.1, 500.0)
-    app.Scene.Camera.Transform.Rotation[1] = 130.0
+	/* create a camera */
+
+	width, height := app.Window.GetBufferSize()
+	app.Scene.Camera = engine.CreateCamera(-3, 2, -3, float32(width), float32(height), 65.0, 0.1, 500.0)
+	app.Scene.Camera.Transform.Rotation[1] = 130.0
 
 	w := app.Scene.World
-    w.NewPlane(0,1,0,0)
+	w.NewPlane(0, 1, 0, 0)
 
-    obj2 := app.Scene.NewObject(5,0,5)
-    chk2 := game.NewColorChunk(obj2, 32)
-    generateChunk(chk2) // populate with random data
-    chk2.Set(0,0,0, &game.ColorVoxel{ R:255, G:0, B:0 })
-    chk2.Set(1,0,0, &game.ColorVoxel{ R:0, G:255, B:0 })
-    chk2.Set(2,0,0, &game.ColorVoxel{ R:0, G:0, B:255 })
-    chk2.Compute()
-    geom_pass.Material.SetupVertexPointers()
-    app.Scene.Add(obj2)
+	obj2 := app.Scene.NewObject(5, 0, 5)
+	chk2 := game.NewColorChunk(obj2, 32)
+	generateChunk(chk2) // populate with random data
+	chk2.Set(0, 0, 0, &game.ColorVoxel{R: 255, G: 0, B: 0})
+	chk2.Set(1, 0, 0, &game.ColorVoxel{R: 0, G: 255, B: 0})
+	chk2.Set(2, 0, 0, &game.ColorVoxel{R: 0, G: 0, B: 255})
+	chk2.Compute()
+	geom_pass.Material.SetupVertexPointers()
+	app.Scene.Add(obj2)
 
-    game.NewPlacementGrid(obj2)
+	game.NewPlacementGrid(obj2)
 
-    fmt.Println("goworld")
+	fmt.Println("goworld")
 
-    // buffer display window
-    bufferWindow := func(title string, texture *render.Texture, x, y float32, depth bool) {
-        win_color := render.Color{0.15, 0.15, 0.15, 0.8}
-        text_color := render.Color{1, 1, 1, 1}
+	// buffer display window
+	bufferWindow := func(title string, texture *render.Texture, x, y float32, depth bool) {
+		win_color := render.Color{0.15, 0.15, 0.15, 0.8}
+		text_color := render.Color{1, 1, 1, 1}
 
-        win   := app.UI.NewRect(win_color, x, y, 250, 280, -10)
-        label := app.UI.NewText(title, text_color, 0, 0, -21)
-        win.Append(label)
+		win := app.UI.NewRect(win_color, x, y, 250, 280, -10)
+		label := app.UI.NewText(title, text_color, 0, 0, -21)
+		win.Append(label)
 
-        if depth {
-            img := app.UI.NewDepthImage(texture, 0, 30, 250, 250, -20)
-            img.Quad.FlipY()
-            win.Append(img)
-        } else {
-            img := app.UI.NewImage(texture, 0, 30, 250, 250, -20)
-            img.Quad.FlipY()
-            win.Append(img)
-        }
+		if depth {
+			img := app.UI.NewDepthImage(texture, 0, 30, 250, 250, -20)
+			img.Quad.FlipY()
+			win.Append(img)
+		} else {
+			img := app.UI.NewImage(texture, 0, 30, 250, 250, -20)
+			img.Quad.FlipY()
+			win.Append(img)
+		}
 
-        /* attach UI element */
-        app.UI.Append(win)
-    }
+		/* attach UI element */
+		app.UI.Append(win)
+	}
 
-    bufferWindow("Diffuse", geom_pass.Buffer.Diffuse, 30, 30, false)
-    bufferWindow("Normal", geom_pass.Buffer.Normal, 30, 340, false)
-    bufferWindow("Shadowmap", light_pass.Shadows.Output, 30, 650, true)
+	bufferWindow("Diffuse", geom_pass.Buffer.Diffuse, 30, 30, false)
+	bufferWindow("Normal", geom_pass.Buffer.Normal, 30, 340, false)
+	bufferWindow("Shadowmap", light_pass.Shadows.Output, 30, 650, true)
 
-    /* Render loop */
-    app.UpdateFunc = func(dt float32) {
-        if engine.KeyReleased(engine.KeyF) {
-            fmt.Println("raycast")
+	/* Render loop */
+	app.UpdateFunc = func(dt float32) {
+		if engine.KeyReleased(engine.KeyF) {
+			fmt.Println("raycast")
 			w.Raycast(10, app.Scene.Camera.Position, app.Scene.Camera.Forward)
-        }
-    }
+		}
+	}
 
-    app.Run()
+	app.Run()
 }
 
 func generateChunk(chk *game.ColorChunk) {
-    /* Define voxels */
-    rock2 := &game.ColorVoxel {
-        R: 200,
-        G: 179,
-        B: 112,
-    }
-    rock := &game.ColorVoxel {
-        R: 141,
-        G: 119,
-        B: 72,
-    }
-    grass := &game.ColorVoxel {
-        R: 88,
-        G: 132,
-        B: 69,
-    }
+	/* Define voxels */
+	rock2 := &game.ColorVoxel{
+		R: 137,
+		G: 131,
+		B: 119,
+	}
+	rock := &game.ColorVoxel{
+		R: 173,
+		G: 169,
+		B: 158,
+	}
+	grass := &game.ColorVoxel{
+		R: 122,
+		G: 189,
+		B: 64,
+	}
 
-    /* Fill chunk with voxels */
-    f := 1.0 / 30.0
-    size := chk.Size
-    simplex := opensimplex.New(3001)
-    for z := 0; z < size; z++ {
-        for y := 0; y < size; y++ {
-            for x := 0; x < size; x++ {
-                fx, fy, fz := float64(x) * f, float64(y) * f, float64(z) * f
-                v := simplex.Eval3(fx, fy, fz)
-                var vtype *game.ColorVoxel = nil
-                if y < size / 4 {
-                    vtype = rock2
-                }
-                if y == size / 4 {
-                    vtype = grass
-                }
-                if v < -0.3 {
-                    vtype = rock
-                }
-                chk.Set(x, y, z, vtype)
-            }
-        }
-    }
+	/* Fill chunk with voxels */
+	f := 1.0 / 30.0
+	size := chk.Size
+	simplex := opensimplex.New(3001)
+	for z := 0; z < size; z++ {
+		for y := 0; y < size; y++ {
+			for x := 0; x < size; x++ {
+				fx, fy, fz := float64(x)*f, float64(y)*f, float64(z)*f
+				v := simplex.Eval3(fx, fy, fz)
+				var vtype *game.ColorVoxel = nil
+				if y < size/4 {
+					vtype = rock2
+				}
+				if y == size/4 {
+					vtype = grass
+				}
+				if v < -0.3 {
+					vtype = rock
+				}
+				chk.Set(x, y, z, vtype)
+			}
+		}
+	}
 }
