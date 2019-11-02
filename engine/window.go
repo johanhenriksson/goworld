ffa059419d8bea4a2863b22c7887b125c0781eb6
Johan Henriksson <johanhenriksson@live.com>
2019-11-02 23:31:25 +0100

    add comments to engine.Window

--- engine/window.go
+++ engine/window.go
@@ -14,9 +14,13 @@ func init() {
 	runtime.LockOSThread()
 }
 
+// UpdateCallback defines the window update callback function
 type UpdateCallback func(float32)
+
+// RenderCallback defines the window render callback function
 type RenderCallback func(*Window, float32)
 
+// Window represents the main engine window
 type Window struct {
 	Wnd           *glfw.Window
 	Width         int
@@ -27,6 +31,7 @@ type Window struct {
 	lastFrameTime float64
 }
 
+// CreateWindow creates the main engine window, and the OpenGL
 func CreateWindow(title string, width int, height int) *Window {
 	if err := glfw.Init(); err != nil {
 		log.Fatalln("Failed to initialize glfw:", err)
@@ -70,53 +75,69 @@ func CreateWindow(title string, width int, height int) *Window {
 	return w
 }
 
+// Closed returns true if the window will close.
 func (wnd *Window) Closed() bool {
 	return wnd.Wnd.ShouldClose()
 }
 
+// SetMaxFps adjusts the maximum frame rate.
 func (wnd *Window) SetMaxFps(fps int) {
 	wnd.maxFrameTime = 1.0 / float64(fps)
 }
 
+// LockCursor locks cursor movement.
 func (wnd *Window) LockCursor() {
 	wnd.Wnd.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
 }
 
+// ReleaseCursor unlocks cursor movement.
 func (wnd *Window) ReleaseCursor() {
 	wnd.Wnd.SetInputMode(glfw.CursorMode, glfw.CursorNormal)
 }
 
+// SetRenderCallback sets the engine render callback.
 func (wnd *Window) SetRenderCallback(cb RenderCallback) {
 	wnd.renderCb = cb
 }
 
+// SetUpdateCallback sets the engine update callback.
 func (wnd *Window) SetUpdateCallback(cb UpdateCallback) {
 	wnd.updateCb = cb
 }
 
+// Loop runs the main engine loop.
 func (wnd *Window) Loop() {
 	for !wnd.Closed() {
+		// calculate frame delta time
 		t := glfw.GetTime()
 		dt := float32(t - wnd.lastFrameTime)
 		wnd.lastFrameTime = t
 
+		// todo: this is part of the FPS camera.
+		// move it somewhere more appropriate
 		if MouseDown(MouseButton1) {
 			wnd.LockCursor()
 		} else {
 			wnd.ReleaseCursor()
 		}
 
+		// update scene
 		if wnd.updateCb != nil {
 			wnd.updateCb(dt)
 		}
 
+		// render scene
 		if wnd.renderCb != nil {
 			wnd.renderCb(wnd, dt)
 		}
 
+		// end scene
+		wnd.Wnd.SwapBuffers()
+		glfw.PollEvents()
+
 		UpdateMouse(dt)
 
-		wnd.EndFrame()
+		// wait a bit if we're running faster than the maximum fps
 		if wnd.maxFrameTime > 0 {
 			elapsed := glfw.GetTime() - t
 			dur := wnd.maxFrameTime - elapsed
@@ -125,19 +146,17 @@ func (wnd *Window) Loop() {
 	}
 }
 
-func (wnd *Window) EndFrame() {
-	wnd.Wnd.SwapBuffers()
-	glfw.PollEvents()
-}
-
+// Terminate kills the GLFW window.
 func (wnd *Window) Terminate() {
 	glfw.Terminate()
 }
 
+// GetBufferSize returns the window framebuffer size.
 func (wnd *Window) GetBufferSize() (int, int) {
 	return wnd.Wnd.GetFramebufferSize()
 }
 
+// Scale returns the window DPI scale relative to the framebuffer.
 func (wnd *Window) Scale() float32 {
 	fw, _ := wnd.Wnd.GetFramebufferSize()
 	return float32(fw) / float32(wnd.Width)
