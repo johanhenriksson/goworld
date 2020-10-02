5ccfe15df03a424bd9d088ce3f3d2c683ae0d899
Johan Henriksson <johanhenriksson@live.com>
2020-10-03 01:22:59 +0200

    add plane geometry

---
+++ geometry/plane.go
@@ -0,0 +1,55 @@
+package geometry
+
+import (
+	"github.com/johanhenriksson/goworld/assets"
+	"github.com/johanhenriksson/goworld/engine"
+	"github.com/johanhenriksson/goworld/math/vec3"
+	"github.com/johanhenriksson/goworld/render"
+)
+
+type Plane struct {
+	*engine.Mesh
+	Size  float32
+	Color render.Color
+}
+
+func NewPlane(parent *engine.Object, size float32, color render.Color) *Plane {
+	mat := assets.GetMaterialCached("vertex_color")
+	plane := &Plane{
+		Mesh:  engine.NewMesh(parent, mat),
+		Size:  size,
+		Color: color,
+	}
+	plane.Pass = render.ForwardPass
+	plane.generate()
+	return plane
+}
+
+func (p *Plane) generate() {
+	s := p.Size / 2
+	o := ColorVertex{
+		Position: vec3.New(-s, 0, -s),
+		Normal:   vec3.UnitY,
+		Color:    p.Color,
+	}
+	x := ColorVertex{
+		Position: vec3.New(s, 0, -s),
+		Normal:   vec3.UnitY,
+		Color:    p.Color,
+	}
+	z := ColorVertex{
+		Position: vec3.New(-s, 0, s),
+		Normal:   vec3.UnitY,
+		Color:    p.Color,
+	}
+	d := ColorVertex{
+		Position: vec3.New(s, 0, s),
+		Normal:   vec3.UnitY,
+		Color:    p.Color,
+	}
+	data := ColorVertices{
+		o, z, x, x, z, d,
+		x, z, o, d, z, x,
+	}
+	p.Buffer("geometry", data)
+}
