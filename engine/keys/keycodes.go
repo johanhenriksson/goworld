0aa21eaf70b5869cbf6e3f4afd36b2c797ecb991
Johan Henriksson <johanhenriksson@live.com>
2020-10-18 23:36:38 +0200

    extract key code constants to their own file

---
+++ engine/keys/keycodes.go
@@ -0,0 +1,42 @@
+package keys
+
+import (
+	"github.com/go-gl/glfw/v3.1/glfw"
+)
+
+// GLFW Keycodes
+const (
+	A Code = 65
+	B Code = 66
+	C Code = 67
+	D Code = 68
+	E Code = 69
+	F Code = 70
+	G Code = 71
+	H Code = 72
+	I Code = 73
+	J Code = 74
+	K Code = 75
+	L Code = 76
+	M Code = 77
+	N Code = 78
+	O Code = 79
+	P Code = 80
+	Q Code = 81
+	R Code = 82
+	S Code = 83
+	T Code = 84
+	U Code = 85
+	V Code = 86
+	W Code = 87
+	X Code = 88
+	Y Code = 89
+	Z Code = 90
+
+	Enter       = Code(glfw.KeyEnter)
+	Escape      = Code(glfw.KeyEscape)
+	Backspace   = Code(glfw.KeyBackspace)
+	Space       = Code(glfw.KeySpace)
+	LeftShift   = Code(glfw.KeyLeftShift)
+	LeftControl = Code(glfw.KeyLeftControl)
+)
