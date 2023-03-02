ba24f157a7da07ea4eac6593d730e55f6ed7efd1
Johan Henriksson <johanhenriksson@live.com>
2023-03-03 00:13:18 +0100

    fix UseEffect bug causing callback to run every frame after change

--- gui/hooks/hooks.go
+++ gui/hooks/hooks.go
@@ -26,21 +26,32 @@ func UseEffect(callback Effect, deps ...any) {
 	state := getState()
 	index := state.Next()
 
+	// if we have no dependencies, run the callback every frame
 	noDeps := len(deps) == 0
+	if noDeps {
+		callback()
+		return
+	}
+
+	// check if any dependencies have changed
 	changed := false
 	if len(state.data) > index {
+		// previous state exists - compare it
 		prev := state.data[index].([]any)
 		for i, dep := range deps {
 			if prev[i] != dep {
 				changed = true
-				break
+				// update previous value so that the callback wont run again next time
+				prev[i] = dep
 			}
 		}
 	} else {
+		// no previous state exists yet
 		state.data = append(state.data, deps)
 	}
 
-	if noDeps || changed {
+	// finally, run the callback if anything changed
+	if changed {
 		callback()
 	}
 }
