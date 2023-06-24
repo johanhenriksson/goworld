2681185a23013b1743615497ab92e5b88f08fbfd
Johan Henriksson <johanhenriksson@live.com>
2023-06-25 01:52:10 +0200

    feat(game): player keyboard turning

--- game/player/player.go
+++ game/player/player.go
@@ -3,6 +3,7 @@ package player
 import (
 	"github.com/johanhenriksson/goworld/core/input/keys"
 	"github.com/johanhenriksson/goworld/core/object"
+	"github.com/johanhenriksson/goworld/math/quat"
 	"github.com/johanhenriksson/goworld/math/vec3"
 	"github.com/johanhenriksson/goworld/physics"
 )
@@ -12,6 +13,7 @@ type T struct {
 	Character *physics.Character
 	Camera    *ArcballCamera
 	Speed     float32
+	TurnRate  float32
 
 	keys     keys.State
 	velocity vec3.T
@@ -22,30 +24,17 @@ func New() *T {
 		Character: physics.NewCharacter(1.8, 0.5, 0.2),
 		Camera:    NewEye(),
 		Speed:     0.05,
+		TurnRate:  40,
 		keys:      keys.NewState(),
 	})
 }
 
+func (p *T) Name() string {
+	return "Player"
+}
+
 func (p *T) Update(scene object.T, dt float32) {
 	p.T.Update(scene, dt)
-	if !p.Character.Grounded() {
-		p.velocity.Scale(1 - 0.9*dt)
-	}
-	p.Character.Move(p.velocity)
-}
-
-func (p *T) Move(dir vec3.T) {
-	if p.Character.Grounded() {
-		p.velocity = dir
-	}
-}
-
-func (p *T) KeyEvent(e keys.Event) {
-	p.keys.KeyEvent(e)
-
-	if p.Character.Grounded() && e.Code() == keys.Space {
-		p.Character.Jump()
-	}
 
 	forward, right := float32(0), float32(0)
 	if p.keys.Down(keys.D) {
@@ -61,6 +50,18 @@ func (p *T) KeyEvent(e keys.Event) {
 		forward -= 1
 	}
 
+	rotate := float32(0)
+	if p.keys.Down(keys.LeftArrow) {
+		rotate -= 1
+	}
+	if p.keys.Down(keys.RightArrow) {
+		rotate += 1
+	}
+
+	rot := p.Camera.Transform().Rotation().Euler()
+	rot.Y += rotate * p.TurnRate * dt
+	p.Camera.Transform().SetRotation(quat.Euler(rot.X, rot.Y, rot.Z))
+
 	camFwd := p.Camera.Transform().Forward()
 	camFwd.Y = 0
 	camFwd.Normalize()
@@ -71,6 +72,20 @@ func (p *T) KeyEvent(e keys.Event) {
 
 	dirForward := camFwd.Scaled(forward)
 	dirRight := camRight.Scaled(right)
-	dir := dirForward.Add(dirRight)
-	p.Move(dir.Scaled(p.Speed))
+	dir := dirForward.Add(dirRight).Normalized()
+
+	if p.Character.Grounded() {
+		p.velocity = dir.Scaled(p.Speed)
+	} else {
+		p.velocity.Scale(1 - 0.9*dt)
+	}
+	p.Character.Move(p.velocity)
+}
+
+func (p *T) KeyEvent(e keys.Event) {
+	p.keys.KeyEvent(e)
+
+	if p.Character.Grounded() && e.Code() == keys.Space {
+		p.Character.Jump()
+	}
 }
