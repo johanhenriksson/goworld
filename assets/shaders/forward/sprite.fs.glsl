1a2747387936c641ff7d886d884ef3dbdb609910
Johan Henriksson <johanhenriksson@live.com>
2023-11-08 00:52:42 +0100

    fix(core): enable lighting for sprites

--- assets/shaders/forward/sprite.fs.glsl
+++ assets/shaders/forward/sprite.fs.glsl
@@ -2,10 +2,12 @@
 #extension GL_GOOGLE_include_directive : enable
 
 #include "lib/common.glsl"
+#include "lib/lighting.glsl"
 #include "lib/forward_fragment.glsl"
 
 CAMERA(0, camera)
 STORAGE_BUFFER(1, Object, objects)
+LIGHT_BUFFER(2, lights)
 SAMPLER_ARRAY(3, textures)
 
 void main() 
@@ -18,7 +20,13 @@ void main()
 		discard; 
 	}
 
+	int lightCount = lights.settings.Count;
+	vec3 lightColor = ambientLight(lights.settings, 1);
+	for(int i = 0; i < lightCount; i++) {
+		lightColor += calculateLightColor(lights.item[i], in_world_position, in_world_normal, in_view_position.z, lights.settings);
+	}
+
     // gamma correct & write fragment
 	vec3 linearColor = pow(albedo.rgb, vec3(gamma));
-    out_diffuse = vec4(linearColor, albedo.a);
+    out_diffuse = vec4(linearColor * lightColor, albedo.a);
 }
