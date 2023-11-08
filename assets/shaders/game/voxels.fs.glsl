04fc77f708056acd6a0c6c2ccf7e6281d6255932
Johan Henriksson <johanhenriksson@live.com>
2023-11-08 12:25:41 +0100

    feat(game): voxel noise texture

--- assets/shaders/game/voxels.fs.glsl
+++ assets/shaders/game/voxels.fs.glsl
@@ -4,9 +4,25 @@
 #include "lib/common.glsl"
 #include "lib/deferred_fragment.glsl"
 
+STORAGE_BUFFER(1, Object, objects)
+SAMPLER_ARRAY(3, textures)
+
+IN(4, vec2, texcoord0)
+IN(5, flat uint, texture_id)
+
+const int tile_count = 8;
+
 void main() 
 {
-	out_diffuse = vec4(in_color.rgb * in_color.a, 1);
+	uint texture0 = objects.item[in_object].textures[0];
+
+	// unpack tile coordinates
+	vec2 t = vec2((in_texture_id + in_texcoord0.x) / tile_count, in_texcoord0.y);
+
+	vec4 tint = vec4(in_color.rgb * in_color.a, 1);
+	vec4 tex = vec4(texture(textures[texture0], t).rgb, 1);
+	out_diffuse =tex * tint;
+
 	out_normal = pack_normal(in_normal);
 	out_position = vec4(in_position, 1);
 }
