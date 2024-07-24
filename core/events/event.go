b535e90b52e126ca356d016c9b851b1524e3061d
Johan Henriksson <johanhenriksson@live.com>
2024-07-24 18:22:29 +0200

    fix(core): reuse event subscriber array slots

--- core/events/event.go
+++ core/events/event.go
@@ -2,10 +2,8 @@ package events
 
 type Data any
 
-type Handler[T Data] func(T)
-
 type Event[T Data] struct {
-	callbacks []Handler[T]
+	callbacks []func(T)
 }
 
 func New[T Data]() Event[T] {
@@ -20,11 +18,26 @@ func (e Event[T]) Emit(event T) {
 	}
 }
 
-func (e *Event[T]) Subscribe(handler Handler[T]) func() {
-	id := len(e.callbacks)
-	e.callbacks = append(e.callbacks, handler)
-	return func() {
-		// unsub
-		e.callbacks[id] = nil
+func (e *Event[T]) Subscribe(handler func(T)) func() {
+	unsubscriber := func(id int) func() {
+		called := false
+		return func() {
+			if called {
+				// its not safe to call the unsubscriber multiple times since the id might be reused
+				panic("unsubscriber called multiple times")
+			}
+			e.callbacks[id] = nil
+			called = true
+		}
 	}
+
+	for id, callback := range e.callbacks {
+		if callback == nil {
+			e.callbacks[id] = handler
+			return unsubscriber(id)
+		}
+	}
+
+	e.callbacks = append(e.callbacks, handler)
+	return unsubscriber(len(e.callbacks) - 1)
 }
