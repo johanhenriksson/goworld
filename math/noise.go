a9ddf0662db6d5a8d7c1289e8cdd056ae472b4df
Johan Henriksson <johanhenriksson@live.com>
2024-08-16 23:13:26 +0200

    feat(math): improved simplex noise api

--- math/noise.go
+++ math/noise.go
@@ -6,23 +6,33 @@ import (
 
 // Noise utility to sample simplex noise
 type Noise struct {
-	opensimplex.Noise
-	Seed int
-	Freq float32
+	Seed  int
+	Scale float32
+	Freq  float32
+
+	simplex opensimplex.Noise
 }
 
 // NewNoise creates a new noise struct from a seed value and a frequency factor.
-func NewNoise(seed int, freq float32) *Noise {
+func NewNoise(seed int, scale, freq float32) *Noise {
 	return &Noise{
-		Noise: opensimplex.New(int64(seed)),
 		Seed:  seed,
+		Scale: scale,
 		Freq:  freq,
+
+		simplex: opensimplex.New(int64(seed)),
 	}
 }
 
-// Sample the noise at a certain position
-func (n *Noise) Sample(x, y, z int) float32 {
+// Sample the 2D noise at a certain position
+func (n *Noise) Sample2(x, z int) float32 {
+	fx, fz := float64(float32(x)*n.Freq), float64(float32(z)*n.Freq)
+	return n.Scale * float32(n.simplex.Eval2(fx, fz))
+}
+
+// Sample the 3D noise at a certain position
+func (n *Noise) Sample3(x, y, z int) float32 {
 	// jeez
 	fx, fy, fz := float64(float32(x)*n.Freq), float64(float32(y)*n.Freq), float64(float32(z)*n.Freq)
-	return float32(n.Eval3(fx, fy, fz))
+	return n.Scale * float32(n.simplex.Eval3(fx, fy, fz))
 }
