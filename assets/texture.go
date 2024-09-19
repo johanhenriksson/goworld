6e65c23108dfcd7dd731102ebfe28a414658b3c1
Johan Henriksson <johanhenriksson@live.com>
2024-09-19 12:03:55 +0200

    refactor(engine): improved assets errors

--- assets/texture.go
+++ assets/texture.go
@@ -10,7 +10,11 @@ import (
 type Texture interface {
 	Asset
 
-	// LoadImage is called by texture caches and loaders, and should return the image data.
+	// LoadTexture is called by texture caches and loaders, and should return the texture data.
+	// Its unfortunate that this method cant return the texture itself directly, since it
+	// requires access to a graphics queue. The texture upload logic must be centralized somewhere.
+	// Techincally not different for meshes? hmm
+	// Should there be a concept on a cpu-side texture? Similar to vertex.Mesh?
 	LoadTexture(assets fs.Filesystem) *texture.Data
 }
 
